f010c6cf2c4ca1bd7e6a4985e6a36c0a6a206a1e
khoamai291299 <khoamai2912@gmail.com>
2025-12-01 23:55:55 +0700

    update myapp.py

--- myapp.py
+++ myapp.py
@@ -2,3 +2,16 @@ def ktra3canhtamgiac(a, b, c):
     if (a + b > c) and (a + c > b) and (b + c > a):
         return 1
     return 0
+
+def ktratamgiac(a, b, c):
+    if ktra3canhtamgiac(a, b, c) == 1:
+        if (a == b) or (a == c) or (b == c):
+            if a == b == c:
+                return 1
+            else:
+                return 2
+        else:
+            if (a*a + b*b == c*c) or (a*a + c*c == b*b) or (c*c + b*b == a*a):
+                return 3
+    else:
+        return 0
